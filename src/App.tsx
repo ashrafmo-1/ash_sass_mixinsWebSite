52e0509469f42c9d8eb9a1fa37597e3ac4a644d2
ashrafmo-1 <ashrafmohammed1176@gmail.com>
2024-08-08 11:26:30 +0300

    start handle files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+function App() {
+
+  return (
+    <div className="web-app">
+      <div className="linksTEST">
+      <a href="">hellw sass mixins {"=>"} 1</a>
+      <a href="">hellw sass mixins {"=>"} 2</a>
+      <a href="">hellw sass mixins {"=>"} 3</a>
+      <a href="">hellw sass mixins {"=>"} 4</a>
+      <a href="">hellw sass mixins {"=>"} 5</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
